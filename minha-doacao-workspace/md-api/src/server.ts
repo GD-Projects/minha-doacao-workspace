5f64dbcaa55f268d347895b70d6775973392e82b
Ruan <devd8727@gmail.com>
2021-10-15 17:25:13 -0300

    initial commit

--- minha-doacao-workspace/md-api/src/server.ts
+++ minha-doacao-workspace/md-api/src/server.ts
@@ -1,1 +1,7 @@
+var express = require('express');
+var app = express();
+app.listen(3001);
+app.get('/teste', (req: any, res: any) => {
+    res.sendFile(__dirname + '/teste.html')
+})
 console.log('Hello Galinha da Agua!')
