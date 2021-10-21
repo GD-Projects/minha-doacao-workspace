3559688f716d6ca77c028789ea4139589e6ade0e
Ruan <devd8727@gmail.com>
2021-10-21 18:09:47 -0300

    footer responsiveness working

--- minha-doacao-workspace/md-web/src/Components/Footer/index.tsx
+++ minha-doacao-workspace/md-web/src/Components/Footer/index.tsx
@@ -11,62 +11,66 @@ import ssl from '../../assets/ssl.png';
 function Footer() {
   return (
     <S.Container>
-      <S.leftContainer>
-        <S.logo>
+      <S.LeftContainer>
+        <S.Logo>
           <img src={logoFooter} alt="icone do site no rodapé" />
           <h2>
             Minha
             <br />
             Doação
           </h2>
-        </S.logo>
-        <S.social>
+        </S.Logo>
+        <S.Social>
           <img src={instagram} alt="icone do instagram" />
           <img src={facebook} alt="icone do facebook" />
           <img src={linkedin} alt="icone do linkedin" />
           <img src={youtube} alt="icone do youtube" />
-        </S.social>
-      </S.leftContainer>
+          <p>Contatos</p>
+        </S.Social>
+      </S.LeftContainer>
 
-      <S.centerContainer>
-        <S.firstText>
-          <p>Contato</p>
-          <p>Quero doar</p>
-          <p>Quem somos</p>
-          <p>Como funciona</p>
-        </S.firstText>
-        <S.secondText>
-          <p>ONGs associadas</p>
-          <p>Empresas parceiras</p>
-          <p>Trabalhe conosco</p>
-          <p>Termos do site</p>
-        </S.secondText>
-        <S.safeIcons>
+      <S.CenterContainer>
+        <S.CenterTextContainer>
+          <S.FirstText>
+            <p>Contato</p>
+            <p>Quero doar</p>
+            <p>Quem somos</p>
+            <p>Como funciona</p>
+          </S.FirstText>
+          <S.SecondText>
+            <p>ONGs associadas</p>
+            <p>Empresas parceiras</p>
+            <p>Trabalhe conosco</p>
+            <p>Termos do site</p>
+          </S.SecondText>
+        </S.CenterTextContainer>
+
+        <S.SafeIcons>
           <img src={safeBrowsing} alt="icone de safe browsing" />
           <img src={ssl} alt="icone de navegação segura com ssl" />
-        </S.safeIcons>
-      </S.centerContainer>
+        </S.SafeIcons>
+      </S.CenterContainer>
 
-      <S.rightContainer>
-        <S.causeTitle>
+      <S.RightContainer>
+        <S.CauseTitle>
           <p>Causas</p>
-        </S.causeTitle>
-        <S.textContainer>
-          <S.firstRightText>
+        </S.CauseTitle>
+        <S.TextContainer>
+          <S.RightFirstText>
             <p>Idosos</p>
             <p>Covid-19</p>
             <p>LGBTQI+</p>
             <p>Educação</p>
-          </S.firstRightText>
-          <S.secondRightText>
+          </S.RightFirstText>
+          <S.RightSecondText>
             <p>Ambiental</p>
             <p>Lutas raciais</p>
             <p>Lutas de gênero</p>
-            <p>Nós contra a fome</p>
+            <p>Alimentação</p>
 
-          </S.secondRightText>
-        </S.textContainer>
-      </S.rightContainer>
+          </S.RightSecondText>
+        </S.TextContainer>
+      </S.RightContainer>
 
     </S.Container>
   );
--- minha-doacao-workspace/md-web/src/Components/Footer/styles.tsx
+++ minha-doacao-workspace/md-web/src/Components/Footer/styles.tsx
@@ -6,27 +6,65 @@ height:38vh;
 background-color: #22577A;
 display: flex;
 justify-content: center;
-padding: 5vh 8vh 5vh 8vh;
+padding: 5vh 1vh 5vh 4vh;
+@media (max-width: 700px) {
+    height:23vh;
+    padding: 0px;
+}
+@media (max-width: 480px) {
+    height:15vh;
+}
+@media (max-width: 405px) {
+    height:12vh;
+}
 `;
 // PRIMEIRO CONTAINER
-export const leftContainer = styled.div`
+export const LeftContainer = styled.div`
 display: flex;
 flex-direction: column;
 justify-content: space-between;
-background-color: red;
-width:28%;
+width:30%;
+@media (max-width: 1000px) {
+    width:28%;
+}
+@media (max-width: 700px) {
+    flex-direction: row;
+    h2 {
+        display: none;
+    }
+}
+@media (max-width: 700px) {
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+
+}
 `;
 
-export const logo = styled.div`
+export const Logo = styled.div`
 display: flex;
+align-items: center;
 color: #F1F2F5;
 height: 50%;
 img {
     padding-right: 10%;
 }
+@media (max-width: 840px) {
+img {
+    height: 70px;
+}
+h2 {
+    font-size: 25px;
+}
+@media (max-width: 580px) {
+    img {
+    height: 55px;
+    }
+}
+}
 `;
 
-export const social = styled.div`
+export const Social = styled.div`
 display: flex;
 align-items: center;
 height: 50%;
@@ -34,44 +72,136 @@ img {
     height: 45%;
     padding-right: 13%;
 }
+p {
+    display: none;
+}
+@media (max-width: 1000px) {
+    img {
+        height: 35%;
+    }
+}
+@media (max-width: 840px) {
+    img {
+        height: 28%;
+    }
+}
+@media (max-width: 700px) {
+    width:80%;
+    justify-content: center;
+    img {
+        height: 70%;
+        padding-left: 5px;
+        padding-right: 25px;
+    }
+    p {
+        display: flex;
+        align-items: center;
+        color: #F1F2F5;
+        font-size: 25px;
+        padding-top: 12px;
+    }
+}
+@media (max-width: 580px) {
+    img {
+        height: 55%;
+        padding-left: 5px;
+        padding-right: 25px;
+    }
+    p {
+        display: flex;
+        color: #F1F2F5;
+        font-size: 20px;
+}
+}
+@media (max-width: 405px) {
+    img {
+        height: 55%;
+        padding-left: 5px;
+        padding-right: 13px;
+    }
+}
 `;
 
 // SEGUNDO CONTAINER
-export const centerContainer = styled.div`
+export const CenterContainer = styled.div`
 display: flex;
 width: 33%;
 flex-wrap: wrap;
 color: #F1F2F5;
-background-color: blue;
+@media (max-width: 1000px) {
+    font-size: 15px;
+}
+@media (max-width: 840px) {
+    font-size: 12px;
+}
+@media (max-width: 700px) {
+    display: none;
+}
 `;
 
-export const firstText = styled.div`
-
+export const CenterTextContainer = styled.div`
+display: flex;
+width: 100%;
 `;
 
-export const secondText = styled.div`
+export const FirstText = styled.div`
+display: flex;
+flex-direction: column;
+line-height: 15px;
+padding-right: 10%;
 `;
 
-export const safeIcons = styled.div`
+export const SecondText = styled.div`
+display: flex;
+flex-direction: column;
+line-height: 15px;
+`;
 
+export const SafeIcons = styled.div`
+display: flex;
+height: 35px;
+width: 45px;
+img {
+    padding-right: 45px;
+}
+@media (max-width: 840px) {
+    height: 30px;
+    width: 35px;
+}
 `;
 
 // TERCEIRO CONTAINER
-export const rightContainer = styled.div`
-width: 30%;
+export const RightContainer = styled.div`
+width: 22%;
 color: #F1F2F5;
-background-color: green;
+@media (max-width: 1000px) {
+    font-size: 15px;
+}
+@media (max-width: 840px) {
+    font-size: 12px;
+}
+@media (max-width: 700px) {
+    display: none;
+}
 `;
-export const causeTitle = styled.div`
+export const CauseTitle = styled.div`
 color:#B0ED4C;
 `;
 
-export const textContainer = styled.div`
+export const TextContainer = styled.div`
 display: flex;
 `;
 
-export const firstRightText = styled.div`
+export const RightFirstText = styled.div`
+display: flex;
+flex-direction: column;
+line-height: 15px;
+padding-right: 10%;
 `;
 
-export const secondRightText = styled.div`
+export const RightSecondText = styled.div`
+display: flex;
+flex-direction: column;
+line-height: 15px;
+padding-right: 10%;
 `;
